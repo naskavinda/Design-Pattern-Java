30b0754101c4aae9195b93aa1d71fa0b376537ee
SUPUN KAVINDA <naskavinda@gmail.com>
2024-05-09 17:03:17 +0000

    initla  commit

---
+++ payslip-generator/src/main/java/com/example/App.java
@@ -0,0 +1,33 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        calculatePayslip(100000);
+    }
+
+    public static void calculatePayslip(double basicSalary) {
+        double allowance = basicSalary * 0.25;
+        double epfDeduction = basicSalary * 0.08;
+        double companyPefContribution = basicSalary * 0.12;
+        double sportClubCharge = 250;
+        double stapDutyCharges = 25;
+
+        double totalSalary = basicSalary + allowance ;
+        double netSalary = totalSalary - epfDeduction - sportClubCharge - stapDutyCharges;
+
+        System.out.println("Basic Salary: $" + basicSalary);
+        System.out.println("Allowance: $" + allowance);
+        System.out.println("EPF Deduction: $" + epfDeduction);
+        System.out.println("Company PEF Contribution: $" + companyPefContribution);
+        System.out.println("Sport Club Charge: $" + sportClubCharge);
+        System.out.println("Stap Duty Charges: $" + stapDutyCharges);
+        System.out.println("Total Salary: $" + totalSalary);
+        System.out.println("Net Salary: $" + netSalary);
+    }
+}
